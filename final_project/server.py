e99aca47591ed20613fff72b4139cf9d13a002ac
Pericles001 <periclesadjovi@gmail.com>
2022-11-01 17:38:14 +0100

    Edit server routes to handle text translation

--- final_project/server.py
+++ final_project/server.py
@@ -1,25 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from markupsafe import escape
+from machinetranslation.translator import english_to_french, french_to_english
 
 app = Flask("Web Translator")
+
+
+@app.route("/")
+def home():
+    """
+    Returns index.htmml
+    :return:
+    """
+    return render_template("index.html")
 
 
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    translation = english_to_french(textToTranslate)
+    return "Translated text to French: {}".format(translation)
 
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translation = french_to_english(textToTranslate)
+    return "Translated text to English : {}".format(translation)
 
 
 @app.route("/")
 def renderIndexPage():
-# Write the code to render template
+    pass
+    # Write the code to render template
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
